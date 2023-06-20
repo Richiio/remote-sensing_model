5e7bc146598b9bb9ae9b75672ef70bd97a7616da
Sarima Chiorlu <chiorlusarima@gmail.com>
2023-06-20 22:07:55 +0100

    Update test_one.py

--- test_one.py
+++ test_one.py
@@ -4,17 +4,15 @@ import model_finetune
 import cv2
 import albumentations as alb
 
-# 模型加载样例
 weights = torch.load(r"output/resnet34-epoch=9-val_acc=0.966.ckpt")["state_dict"] # 模型权重
 for k in list(weights.keys()):
     weights[str(k)[4:]]=weights.pop(k)
 
 net = model_finetune.ResNet("resnet34",30)
-net.load_state_dict(weights) # 加载权重字典
+net.load_state_dict(weights)
 print(net)
 
-# 测试一张图片
-image = cv2.imread(r"D:/Game_lsh/Gloabel_data/AID/Viaduct/viaduct_256.jpg", cv2.IMREAD_COLOR)
+image = cv2.imread(r"D:/Game_lsh/Gloabel_data/Data/Viaduct/viaduct_256.jpg", cv2.IMREAD_COLOR)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 transforms_train = alb.Compose([
         alb.Resize(height=224, width=224, p=1),
